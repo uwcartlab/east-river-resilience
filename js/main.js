36b402eceff18893049f32b8872e7c49e8206bc5
jake-steinberg <98494747+jake-steinberg@users.noreply.github.com>
2023-01-10 11:17:20 -0600

    changes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 require([
     "esri/config",
     "esri/Map",
+    "esri/layers/FeatureLayer",
     "esri/views/MapView"
-], function (esriConfig, Map, MapView) {
+], function (esriConfig, Map, MapView, FeatureLayer) {
 
     //Setting the API key
     esriConfig.apiKey = "AAPK539d9a98e214453db04a93dfaea676adrxOVh3ZE6LGf-_99lThjFmfwvhD2YHTXoC2px7zOQ0x8qZ9nwvwoeuZn0PgmFA_f"
@@ -20,6 +21,13 @@ require([
             xmax: -87.57033074655209,
             ymax: 44.58559644055305,
             spatialReference: 4326
-        }
+        },
     });
+
+    let one_year_high = new FeatureLayer({
+        url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/ArcGIS/rest/services/1_year_high_lake_level/FeatureServer/13"
+    });
+
+    map.add(one_year_high);
+
 });
