1a7ef44bc51fa85951ba88c6e785152a63d93416
jake-steinberg <98494747+jake-steinberg@users.noreply.github.com>
2023-01-12 16:32:04 -0600

    added layers

--- js/main.js
+++ js/main.js
@@ -10,13 +10,15 @@ require([
     "esri/widgets/Slider"
 ], function (esriConfig, Map, MapView, FeatureLayer, Basemap, BasemapGallery, GroupLayer, LayerList, Slider) {
 
-    //Setting the API key
+    //Set the API key
     esriConfig.apiKey = "AAPK539d9a98e214453db04a93dfaea676adrxOVh3ZE6LGf-_99lThjFmfwvhD2YHTXoC2px7zOQ0x8qZ9nwvwoeuZn0PgmFA_f"
 
+    //Set up the basemap
     const map = new Map({
         basemap: "arcgis-topographic"
     });
 
+    //Set up the Map View
     const view = new MapView({
         container: "viewDiv",
         map: map,
@@ -29,6 +31,8 @@ require([
         }
     });
 
+    //Set up the map layers
+    //High Lake Level
     let one_year_high = new FeatureLayer({
         url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/1_year_high_lake_level/FeatureServer/13",
         title: "1-year flood"
@@ -40,10 +44,36 @@ require([
         visible: false
     });
 
+    let one_hundred_year_high = new FeatureLayer({
+        url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/100_year_high_lake_level/FeatureServer/0",
+        title: "100-year flood",
+        visible: false
+    });
+
+    let five_hundred_year_high = new FeatureLayer({
+        url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/500_year_high_lake_level/FeatureServer/0",
+        title: "500-year flood",
+        visible: false
+    });
+    //Low Lake Level
+    let one_year_low = new FeatureLayer({
+        url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/1_year_low_lake_level/FeatureServer/0",
+        title: "1-year flood",
+        visible: false
+    });
+
+    let one_hundred_year_low = new FeatureLayer({
+        url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/100_year_lake_level_low/FeatureServer/0",
+        title: "100-year flood",
+        visible: false
+    });
+
+    //Basemap layers for gallery widget
     let arcgisTopo = Basemap.fromId("arcgis-topographic")
     let arcgisImagery = Basemap.fromId("arcgis-imagery-standard")
     let arcgisHybrid = Basemap.fromId("arcgis-imagery")
 
+    //Set up the basemap gallery widget
     const basemapGallery = new BasemapGallery({
         view: view,
         source: [arcgisTopo, arcgisImagery, arcgisHybrid]
@@ -54,18 +84,34 @@ require([
         position: "top-right"
       });
 
-      const HighGroupLayer = new GroupLayer({
+    //Create group layer for high lake level layers
+    const HighGroupLayer = new GroupLayer({
+        title: "High Lake Level",
+        visible: true,
+        visibilityMode: "exclusive",
+        layers: [one_year_high, ten_year_high, one_hundred_year_high, five_hundred_year_high],
+        opacity: 0.75
+    });
+    //Create group layer for low lake level layers
+    const LowGroupLayer = new GroupLayer({
+        title: "Low Lake Level",
+        visible: true,
+        visibilityMode: "exclusive",
+        layers: [one_year_low, one_hundred_year_low],
+        opacity: 0.75
+    });
+    //Create group layer for all layers
+    const LayerGroupLayer = new GroupLayer({
         title: "Layers",
         visible: true,
         visibilityMode: "exclusive",
-        layers: [one_year_high, ten_year_high],
+        layers: [HighGroupLayer, LowGroupLayer],
         opacity: 0.75
-      });
+    });
 
-      map.add(HighGroupLayer);
+    map.add(LayerGroupLayer);
 
-    // Creates actions in the LayerList.
-
+    //Creates actions in the LayerList.
     async function defineActions(event) {
         // The event object contains an item property.
         // is is a ListItem referencing the associated layer
@@ -170,20 +216,20 @@ require([
             // if the increase-opacity action is triggered, then
             // increase the opacity of the GroupLayer by 0.25
 
-            if (HighGroupLayer.opacity < 1) {
-                HighGroupLayer.opacity += 0.25;
+            if (LayerGroupLayer.opacity < 1) {
+                LayerGroupLayer.opacity += 0.25;
             }
             } else if (id === "decrease-opacity") {
             // if the decrease-opacity action is triggered, then
             // decrease the opacity of the GroupLayer by 0.25
 
-            if (HighGroupLayer.opacity > 0) {
-                HighGroupLayer.opacity -= 0.25;
+            if (LayerGroupLayer.opacity > 0) {
+                LayerGroupLayer.opacity -= 0.25;
             }
             }
         });
 
         // Add widget to the top right corner of the view
-        view.ui.add(layerList, "top-left");
+        view.ui.add(layerList, "top-right");
         });
 });
